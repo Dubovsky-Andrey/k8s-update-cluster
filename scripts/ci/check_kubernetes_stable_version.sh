37a71ceb7d76c4ea150beb87ba32d5ba21f982e2
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-05-16 10:43:25 -0400

    refactoring steps

--- scripts/ci/check_kubernetes_stable_version.sh
+++ scripts/ci/check_kubernetes_stable_version.sh
@@ -3,5 +3,3 @@
 LATEST=$(curl -fsSL https://dl.k8s.io/release/stable.txt)
 echo "k8s_latest=$LATEST"
 echo "k8s_latest=$LATEST" >> $GITHUB_OUTPUT
-echo "### Kubernetes version report" >> $GITHUB_STEP_SUMMARY
-echo "- Latest stable release: ${{ steps.fetch_k8s.outputs.k8s_latest }}" >> $GITHUB_STEP_SUMMARY
